e39799d45882a29962175efe6a586e60ff6e265e
Tristan Viney <yenivyeniv@gmail.com>
2019-05-21 10:34:43 +0200

    removed extra pw column from users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_161520) do
+ActiveRecord::Schema.define(version: 2019_05_21_083326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,7 +49,6 @@ ActiveRecord::Schema.define(version: 2019_05_20_161520) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "password"
     t.string "first_name"
     t.string "last_name"
     t.string "profile_picture"
