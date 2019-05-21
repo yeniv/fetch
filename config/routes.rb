f72fd0a55453a3b9d856d29782e7521d742fbc1d
maxsalemans <mhrsalemans@gmail.com>
2019-05-21 12:26:21 +0200

    added routes + Dogs show, index

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,31 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :dogs, only: [:index, :home, :show, :new] do
+    resources :appointments, only: [:create]
+  end
+
+  resources :appointments, only: [:show] do
+    member do
+      patch :deny
+      patch :accept
+    end
+  end
+
+  get 'profile', to: 'users#show'
+
 end
+
+
+# get 'profile', to: 'users#show'
+
+
+
+
+# /dogs
+# dogs/new
+# dogs/id
+# dogs/id/appointments
+# appointments/id/deny-accept
+# /profile
