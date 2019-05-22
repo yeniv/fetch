dbe052e37e9a109bf6f28778c967f76c08a5e14b
Tristan Viney <yenivyeniv@gmail.com>
2019-05-22 20:44:35 +0200

    notification cards now load correctly

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,15 @@
 class ProfilesController < ApplicationController
   def show
     @user = current_user
-    @appointments = @user.appointments
+
+    # appointments the user has made
+    @appointments_made = @user.appointments
+
+    # appointments the user has recieved
+    @appointments_recieved = Appointment.where(dog_id: @user.dogs)
+  end
+
+  def status_color
+    "Hello"
   end
 end
