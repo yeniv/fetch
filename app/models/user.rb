3d53999e346d4c643729ac8eb51b0b928fbcb9ff
Max Teunissen <max.teunissen@mac.com>
2019-05-23 11:55:55 +0200

    only added a validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,5 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
